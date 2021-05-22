7c2376b5a3447b9178cf17606883be8572e96394
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2021-05-22 18:22:33 +0530

    Revert "recur_2over"
    This reverts commit 1e26eed28f4dea6843ecde60efd53290c9c630d6.

--- tower_oh_hanoi.py
+++
@@ -1,8 +0,0 @@
-def tower(n, a, b, c):
-    if n == 1:
-        print("move 1st from", a, "to", c)
-        return
-    tower(n - 1, a, c, b)
-    print("move", n, "the disk from", a, "to", c)
-    tower(n - 1, b, a, c)
-print(tower(4, "s", "a", "d"))
